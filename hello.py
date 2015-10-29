796715df3f6f82ccd9e02b030496885819f783f6
mkome <mimi@students.olin.edu>
2015-10-29 17:49:44 -0400

    hello.py: add a comment and two print statements
    learned about resets, hello now finds the differece between random integers.

---
+++ hello.py
@@ -0,0 +1,11 @@
+##This file is for learning about GIT!
+from random import randint
+
+print "hello! " + "a short python program"
+x = randint(0,5)
+print x
+
+print "another small change"
+
+y = randint(0,5)
+print x-y
