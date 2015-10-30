70296fd909b9c6d8c9d844e4dbe566d5e5387c22
mkome <mimi@students.olin.edu>
2015-10-30 11:21:10 -0400

    rebasepractice.py: add some filler code so I can try out rebase
    Started writing the lines for a randomly generated story under the counter.

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 ##This file is for learning about GIT!
 from random import randint
 
+#Mostly it is print statements
 print "hello! " + "a short python program"
+
+#there are also random numbers happening
 x = randint(0,5)
 print x
 
@@ -9,3 +12,13 @@ print "another small change"
 
 y = randint(0,5)
 print x-y
+
+#We also have some probability happening
+if x == 3 or y == 3:
+	print "both were three! what's the chance of that?"
+	chance = (1.0/6.0)*(1.0/6.0)
+	print "chance = " + str(chance)
+if x > y:
+	print "x was greater than y! what's the chance of that?"
+	print "chance = figure it out yourself I'm busy"
+
---
+++ rebasepractice.py
@@ -0,0 +1,20 @@
+##this is a place to practice git rebase
+counter = 1
+while counter <= 10:
+	print counter 
+	counter+=1
+
+#some silly filler code from POE
+gabe = False
+while gabe == False:
+	print "Gaaaaabe"
+	gabe = True
+	if gabe:
+		print "I forgive you for making everything terrible. it happens. you're only gabe, after all."
+
+#a randomly generated story
+bh1 = "once upon a time there was a boring human"
+bh2 = "boring human was flat. like, really flat. like only two dimensional flat."
+bh3 = "what's worse, boring human could only move in one dimension"
+bh4 = "boring human wanted to get from left to right"
+bh5 = ""
