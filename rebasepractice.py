42c06cbdd493dd61eaba1ef9ee5134893ede5e94
mkome <mimi@students.olin.edu>
2015-10-30 11:35:44 -0400

    rebasepractice.py: Add more lines for the randomly generated story
    Added some more story elements and made some typo edits. Commented out some old
    code that I don't want right now, but might want to use in the future.

--- rebasepractice.py
+++ rebasepractice.py
@@ -13,8 +13,16 @@ while gabe == False:
 		print "I forgive you for making everything terrible. it happens. you're only gabe, after all."
 
 #a randomly generated story
-bh1 = "once upon a time there was a boring human"
-bh2 = "boring human was flat. like, really flat. like only two dimensional flat."
-bh3 = "what's worse, boring human could only move in one dimension"
-bh4 = "boring human wanted to get from left to right"
-bh5 = ""
+bh1 = "Once upon a time there was a boring human."
+bh2 = "This boring human was flat. Like, really flat. Like only two dimensional flat."
+bh3 = "What's worse, this boring human could only move in one dimension."
+bh4 = "This boring human wanted to get from left to right."
+bh5 = "Of course the boring human wanted that. All humans want to be right and no human wants to be left."
+bh6 = "So, we've established that this boring human is nothing special, and we don't need to finish this story."
+
+p1 = "Once upon a time there was a princess."
+p2 = "This princess was proportionally normal, but extremely big. Like really really big. 10x person here."
+p3 = "This princess could do anything she wanted, except things that were size specific. You know, the world is very sizeist."
+p4 = "This princess wanted to be able to interact with the world she lived in, without just crushing everything."
+p5 = "Of course she did. That's a perfectly reasonable thing to want."
+p6 = "But we know that you can't have everything you want, and this was just unrealistic, so that's the whole story."
